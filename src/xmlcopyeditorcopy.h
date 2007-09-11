d3efe4d1e5401e125b0b91fec51a9a9384c1a6ac
Justin Dearing <j-pimp@users.sourceforge.net>
2007-09-11 23:14:06 +0000

    Added the GPL Copyright notice to all cpp and h files.

--- src/xmlcopyeditorcopy.h
+++ src/xmlcopyeditorcopy.h
@@ -1,3 +1,22 @@
+/* 
+ * Copyright 2005-2007 Gerald Schmidt.
+ * 
+ * This file is part of Xml Copy Editor.
+ * 
+ * Xml Copy Editor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ * 
+ * Xml Copy Editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Xml Copy Editor; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #define ENCODING_INFO _T("UTF-8, UTF-16, UTF-16LE, UTF-16BE, ISO-8859-1, US-ASCII")
 #define FILE_FILTER _("All files (*.*)|*.*|XML (*.xml)|*.xml|XHTML (*.html)|*.html|DTD (*.dtd)|*.dtd|XML Schema (*.xsd)|*.xsd|RELAX NG grammar (*.rng)|*.rng|XSL (*.xsl)|*.xsl")
 #define ABOUT_COPYRIGHT _("Copyright © 2005-2007 Gerald Schmidt <gnschmidt@users.sourceforge.net>")
