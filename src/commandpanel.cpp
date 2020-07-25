594377896856dc68ed703e362494cd0e9c3b2280
Zane U. Ji <zaneuji@gmail.com>
2020-07-25 16:34:10 +0800

    Fix a compilation error

--- src/commandpanel.cpp
+++ src/commandpanel.cpp
@@ -18,6 +18,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include <wx/wx.h>
 #include <wx/utils.h>
 #include "commandpanel.h"
 #include "xmlcopyeditor.h"
